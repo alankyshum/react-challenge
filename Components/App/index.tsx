c4148ad7e5be4437085e28ef972dbb0ddb3d14db
Alan Shum <alan200994@gmail.com>
2018-08-06 23:37:06 -0700

    basic structure

--- Components/App/index.tsx
+++ Components/App/index.tsx
@@ -1,7 +1,33 @@
 import * as React from 'react';
 
+class Checkbox extends React.Component {
+  render() {
+    return <div>
+      <input type="checkbox"/>
+      <button>Remove</button>
+    </div>;
+  }
+}
+
 export default class App extends React.Component {
   render() {
-    return <div>testing</div>;
+    return <>
+      <div>
+        <input type="checkbox"/>
+        <span>Check All</span>
+      </div>
+      <button>Add</button>
+      { this.generateChecklist(10) }
+    </>
+  }
+
+  generateChecklist(itemsCount: number): React.ReactElement<Checkbox>[] {
+    const checklist: React.ReactElement<Checkbox>[] = [];
+
+    for (let i = 0; i < itemsCount; i++) {
+      checklist.push(<Checkbox key={ i }/>)
+    }
+
+    return checklist;
   }
 }
