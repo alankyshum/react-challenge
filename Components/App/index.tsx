324bb6978ad01d07b7b64de3f15dfa839fbd334c
Alan Shum <alan200994@gmail.com>
2018-08-11 14:07:18 -0700

    reset project

--- Components/App/index.tsx
+++ Components/App/index.tsx
@@ -1,49 +1,29 @@
 import * as React from 'react';
 
-class Checkbox extends React.Component<{ checklistItemID: number; removeItem: { (): void }, checked: boolean }> {
+class Checkbox extends React.Component {
   render() {
     return <div>
-      <input type="checkbox" defaultChecked={ this.props.checked } />
-      <span>{ this.props.checklistItemID }</span>
-      <button onClick={ this.props.removeItem }>Remove</button>
+      <input type="checkbox" />
+      <button>Remove</button>
     </div>;
   }
 }
 
-interface AppProps {
-  checklistIDs: Set<number>;
-  updateChecklist: { (indexToRemove?: number): void; }
-  checkAll: { (): void; }
-  allChecked: boolean;
-}
-
-export default class App extends React.Component<AppProps> {
+export default class App extends React.Component {
   render() {
     return <>
       <div>
-        <input id="check-all" type="checkbox" onChange={ this.checkAll.bind(this) } defaultChecked={ this.props.allChecked }/>
+        <input id="check-all" type="checkbox" />
         <label htmlFor="check-all">Check All</label>
       </div>
-      <button onClick={ this.addItem.bind(this) }>Add</button>
-      { this.generateChecklist(this.props.checklistIDs, this.props.allChecked) }
+      <button>Add</button>
+      { this.generateChecklist(new Set()) }
     </>
   }
 
-  checkAll() {
-    this.props.checkAll();
-  }
-
-  addItem() {
-    this.props.updateChecklist();
-  }
-
-  removeItem(itemIndex: number) {
-    this.props.updateChecklist(itemIndex);
-  }
-
-  generateChecklist(items: Set<number>, allChecked: boolean): React.ReactElement<Checkbox>[] {
+  generateChecklist(items: Set<number>): React.ReactElement<Checkbox>[] {
     return Array.from(items).map(itemID =>
-      <Checkbox key={ itemID } checklistItemID={ itemID } removeItem={ () => this.removeItem(itemID) } checked={ allChecked }/>
+      <Checkbox key={ itemID } />
     );
   }
 }
--- Components/index.tsx
+++ Components/index.tsx
@@ -1,13 +1,7 @@
-import Consumer from './State/Consumer';
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
-import Provider from './State/Provider';
 
 render(
-  <Provider>
-    <Consumer>
-      <App />
-    </Consumer>
-  </Provider>
+  <App />
 , document.getElementById('root'));
